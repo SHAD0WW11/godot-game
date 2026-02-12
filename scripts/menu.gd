ad1cdd852cfa8ca592ef5bc65b3bf6467ff02f78
SHADØW <wozniakandrewj@gmail.com>
2026-02-11 19:12:13 -0500

    settings and stuff

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -7,3 +7,7 @@ func _on_button_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/settings.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
--- scripts/settings.gd
+++ scripts/settings.gd
@@ -1,13 +1,17 @@
 extends Control
 
+func _ready():
+		$"fps cap/Label".text = "fps cap 60"
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
-
-
 
 func _on_check_button_toggled(button_pressed):
 	if button_pressed == true:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func _on_fps_cap_value_changed(value: int) -> void:
+	Engine.max_fps = value
+	$"fps cap/Label".text = "fps cap " + str(value)
