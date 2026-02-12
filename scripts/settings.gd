0894cee2d918cf0266589aaa88a68e2843ba9851
SHADØW <wozniakandrewj@gmail.com>
2026-02-11 22:26:47 -0500

    fixed fullscreen i think

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -1,17 +1,28 @@
 extends Control
 
-func _ready():
-		$"fps cap/Label".text = "fps cap 60"
+var current_max_fps = Engine.max_fps
 
+func _ready():
+		#fps cap label
+		$"fps cap/Label".text = "fps cap " + str(current_max_fps)
+		if $"fps cap/Label".text == "fps cap 0":
+			$"fps cap/Label".text = "fps cap 60"
+		#windowed/fullscreen mode show
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+			#fullscreen
+			$fullscreen.button_pressed = true
+		else:
+			#windowed
+			$fullscreen.button_pressed = false
+#go back to menu
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
-
+#toggles windowed or fullscreen
 func _on_check_button_toggled(button_pressed):
 	if button_pressed == true:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-
+#fps cap
 func _on_fps_cap_value_changed(value: int) -> void:
 	Engine.max_fps = value
-	$"fps cap/Label".text = "fps cap " + str(value)
