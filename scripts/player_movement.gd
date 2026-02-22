a92510614b594c6a4cff8bf7afaafa37c1d7f5d2
SHADØW <wozniakandrewj@gmail.com>
2026-02-21 19:06:16 -0500

    added movement

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,1 +1,11 @@
 extends CharacterBody2D
+var speed: float = 1
+var speed_multi = 250
+
+func get_input():
+	var input_dir = Input.get_vector("left","right","up","down")
+	velocity = input_dir * speed * speed_multi
+
+func _physics_process(delta):
+	get_input()
+	move_and_collide(velocity * delta)
